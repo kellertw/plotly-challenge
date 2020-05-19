fef593776c8c4aa362d36f785311241ebb1c3158
Kellertw <keller.tw@gmail.com>
2020-05-19 16:25:06 -0400

    added bubble, gauge

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,108 +1,117 @@
 var jsonData;
 
-
 int();
 
-
-
 function int() {
-    d3.json('samples.json').then((data) => {
-        jsonData = data;
-        var names = data.names;
-        var select = d3.select('select');
-        names.forEach(name => select.append('option').text(name).attr('value',name));
+  d3.json("samples.json").then((data) => {
+    jsonData = data;
+    var names = data.names;
+    var select = d3.select("select");
+    names.forEach((name) =>
+      select.append("option").text(name).attr("value", name)
+    );
 
-        showPanel(names[0]);
-    });
-};
+    showPanel(names[0]);
+    showBar(names[0]);
+    showBubble(names[0]);
+    showGauge(names[0]);
+  });
+}
 
 function showPanel(name) {
-    d3.json('samples.json').then((data) => {
-        var panel = d3.select('.panel-body');
-        var metadata = data.metadata.filter(obj => obj.id == name)[0];
+  d3.json("samples.json").then((data) => {
+    var panel = d3.select(".panel-body");
+    var metadata = data.metadata.filter((obj) => obj.id == name)[0];
 
-        panel.html('');
+    panel.html("");
 
-        Object.entries(metadata).forEach(([key,value]) => {
-            panel.append('h6').text(`${key.toUpperCase()}: ${value}`);
-        });
+    Object.entries(metadata).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
+  });
+}
+
+function optionChanged(name) {
+  showPanel(name);
+  showBar(name);
+  showBubble(name);
+  showGauge(name);
+}
+
+function showBar(name) {
+  d3.json("samples.json").then((data) => {
+    var sample = data.samples.filter((obj) => obj.id == name)[0];
+
+    var barData = [
+      {
+        x: sample.sample_values.slice(0, 10).reverse(),
+        y: sample.otu_ids
+          .slice(0, 10)
+          .reverse()
+          .map((id) => `OTU ${id}`),
+        text: sample.otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      },
+    ];
+
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 },
+    };
+
+    Plotly.newPlot("bar", barData, barLayout);
+  });
+}
+
+function showBubble(name) {
+  d3.json("samples.json").then((data) => {
+    var sample = data.samples.filter(obj => obj.id == name)[0];
+
+    var bubbleData = [
+      {
+        x: sample.otu_ids,
+        y: sample.sample_values,
+        text: sample.otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample.sample_values,
+          color: sample.otu_ids,
+          colorscale: "Earth",
+        }
+      }
+    ];
+
+    var bubbleLayout = {
+        title: 'Bacteria Cultures Per Sample',
+        margin: {t: 30},
+        hovermode: 'closest',
+        xaxis: {title: 'OTU ID'}
+    };
+
+    Plotly.newPlot('bubble',bubbleData,bubbleLayout);
+  });
 };
 
-function buildPlot(sample) {
-    d3.json('samples.json').then((data) => { 
-        var samples=data.samples;
-        var resultArray=samples.filter(sampleObj => sampleObj.id==sample);
-        var result=resultArray[0];
+function showGauge(name) {
+    d3.json('samples.json').then(data => {
+        var frq = data.metadata.filter(obj => obj.id == name)[0].wfreq;
 
-        var otu_ids=result.otu.ids;
-        var otu_labels=result.otu_labels;
-        var sample_values=result.sample_values;
-        // Bulid the bubble chart
-        var layout = {
-            title: 'OTU IDs Bubble Chart',
-            hovermode:"closest",
-            showlegend: false,
-            height: 600,
-            width: 600
-        };
-        var trace1 = [
-            {
-                x: otu_ids,
-                y: sample_values,
-                text: otu_labels,
-                mode: 'markers',
-                marker: {
-                    color: otu_ids,
-                    size: sample_values
-                }
-            }
-        ];
-    
-        Plotly.newPlot("bubble", trace1, layout);
-    });
-}
-    
+       
         var data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
-                value: data.wfreq,
+                value: frq,
                 title: { text: "Scrubs per Week", font:{size: 14}},
                 type: "indicator",
                 mode: "gauge+number+delta"
             }
         ];
-        
+
         var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
         Plotly.newPlot('gauge', data, layout);
-
-// // int();
-
-
-
-//     // function int() {
-//     //     d3.json('samples.json').then(data => {
-//     //         jsonData = data;
-//     //         var names = data.names;
-//     //         var select = d3.select('select');
-//     //         names.forEach(name => select.append('option').text(name).attr('value',name));
-    
-//     //         showPanel(names[0]);
-//     //     });
-//     // };
-
-    const sample1 = names[0];
-    buildCharts(sample1);
-    buildMetadata(sample1);
-    console.log(sample1)
-    });
+        
+    })
 }
-    
-    function optionChanged(nextsample) {
-      // Fetch new data each time a new sample is selected
-      buildCharts(nextsample);
-      buildMetadata(nextsample);
-    }
-    
-    // Initialize the dashboard
-    init();
+
+
